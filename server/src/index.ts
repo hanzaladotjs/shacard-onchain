d0d2576d68970363fe35edc0b33f5c53a2c8cc8d
hanzaladotjs <bigapplestorekmr@gmail.com>
2025-08-06 01:02:09 +0530

    moving forward

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,10 @@
 import { Hono } from 'hono'
-
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 const app = new Hono()
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle({ client: sql });
+
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
