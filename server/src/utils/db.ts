8b3416b63f15fd971eb6bf05aecb363f5b0ee68d
hanzaladotjs <bigapplestorekmr@gmail.com>
2025-08-21 16:23:17 +0530

    lil comment

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -4,6 +4,9 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
 function getDb(dburi: string) {
+  if(!dburi){
+    return
+  }
   const client =  postgres(dburi);
   return drizzle(client);
 }
