cf85123ecbb672803c84a94175fdbe7f9165fd64
hanzaladotjs <bigapplestorekmr@gmail.com>
2025-08-28 23:19:30 +0530

    landing

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,21 @@
+
+const Home = () => {
+    return (
+        <div>   
+            <div className="animate-stamp">
+              <div className="md:w-[800px] w-[300px] md:text-4xl tracking-wide text-gray-700  mt-20 flex flex-col ">
+                <div className="text-center md:text-6xl text-2xl mb-6">K2K is Knowledge to Knowledge.</div>
+                <div className="text-center">  a platform that connects people with the best resources, knowledge, and expertise.</div>
+              </div>
+
+              <div className="flex justify-center mt-5 text-gray-400 text-xs md:text-lg">
+                barter skills. save money. avoid loneliness.
+              </div>
+              </div>
+            
+
+        </div>
+    )
+}
+
+export default Home
