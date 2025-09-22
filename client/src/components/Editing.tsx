7d05647f39eca9b18802430c38615286972e5d09
hanzalaodotjs <bigapplestorekmr@gmail.com>
2025-09-22 22:42:40 +0530

    nft

--- client/src/components/Editing.tsx
+++ client/src/components/Editing.tsx
@@ -1,8 +1,10 @@
 export default function Editing () {
     return (
-        <div className="h-[500px] w-[400px] rounded-xl p-4 ">
-
-hi
+        <div className="h-[550px] w-[480px] rounded-xl p-4 border-5 border-gray-300 bg-gray-200 flex flex-col items-center">
+            
+            <input placeholder="name" className="h-10 w-full border"/>
+            <input />
+            
         </div>
     )
 }
--- client/src/pages/Mint.tsx
+++ client/src/pages/Mint.tsx
@@ -4,39 +4,60 @@ import html2canvas from "html2canvas"
 import { useRef } from "react"
 
 export default function Mint() {
-    const cardRef = useRef(null)
+    const cardRef = useRef<HTMLDivElement>(null)
 
-    const handleDownload = async () => {
-    if (!cardRef.current) return;
+    const getCardBlob = async (): Promise<Blob | null> => {
+        if (!cardRef.current) return null
 
-    await document.fonts.ready; // wait for Tailwind/webfonts
+        await document.fonts.ready
 
-    const canvas = await html2canvas(cardRef.current, {
-      scale: 2,            // higher DPI
-      backgroundColor: null,
-      useCORS: true,
-    });
+        const canvas = await html2canvas(cardRef.current, {
+            scale: 2,
+            backgroundColor: null,
+            useCORS: true,
+        })
 
-    // create PNG download
-    canvas.toBlob((blob:any) => {
-      const link = document.createElement("a");
-      link.download = "card.png";
-      link.href = URL.createObjectURL(blob);
-      link.click();
-      URL.revokeObjectURL(link.href);
-    });
-  };
+        return new Promise((resolve) => canvas.toBlob(resolve))
+    }
+
+    const pinataURI = import.meta.env.VITE_SERVER_URL
+
+    const handleUpload = async () => {
+        const blob = await getCardBlob()
+        if (!blob) return
+
+        const formData = new FormData()
+        formData.append("file", blob, "card.png")
+
+
+        const res = await fetch(pinataURI, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${import.meta.env.VITE_PINATA_JWT}`, // secure with env var
+            },
+        })
+
+        const { url } = await res.json()
+
+        await fetch(url, {
+            method: "POST",
+            body: formData
+        })
+
+    }
+
     return (
-        <div className="flex space-x-60 px-15 py-20  mt-30 bg-gray-50 rounded-md ">
+        <div className="flex relative space-x-10 px-15 py-20  mt-30 bg-gray-50 rounded-md ">
 
-            <Edit  cardRef={cardRef} />
+            <Edit cardRef={cardRef} />
 
-            <Editing/>
+            <Editing />
 
-            <button onClick={handleDownload} className="text-black"> convert </button>
-            <button> connect</button>
+            <br></br>
+   
+            <button onClick={handleUpload} className="text-gray-100 px-2 py-1 absolute bottom-10 border-3 border-stone-200 bg-black right-50"> upload </button>
             
-            
+
         </div>
     )
 }
